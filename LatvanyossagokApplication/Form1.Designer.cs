f9374414d94b38903eb7c77b08cf1953e6dd09d8
jakus marci <jakus.marton@gmail.com>
2020-12-05 18:53:53 +0100

    Add project files.

---
+++ LatvanyossagokApplication/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace LatvanyossagokApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bVarostHozzaAd = new System.Windows.Forms.Button();
+            this.tBVarosNev = new System.Windows.Forms.TextBox();
+            this.nUADVarosLakosaga = new System.Windows.Forms.NumericUpDown();
+            this.gBVaros = new System.Windows.Forms.GroupBox();
+            this.gBLatvanyosag = new System.Windows.Forms.GroupBox();
+            this.nUDLatvanyosagAr = new System.Windows.Forms.NumericUpDown();
+            this.bLHozzaAd = new System.Windows.Forms.Button();
+            this.cBVaroshozKotes = new System.Windows.Forms.ComboBox();
+            this.tBLatvanyosagNev = new System.Windows.Forms.TextBox();
+            this.tBLatvanyosagLeiras = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nUADVarosLakosaga)).BeginInit();
+            this.gBVaros.SuspendLayout();
+            this.gBLatvanyosag.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nUDLatvanyosagAr)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bVarostHozzaAd
+            // 
+            this.bVarostHozzaAd.Location = new System.Drawing.Point(6, 71);
+            this.bVarostHozzaAd.Name = "bVarostHozzaAd";
+            this.bVarostHozzaAd.Size = new System.Drawing.Size(75, 23);
+            this.bVarostHozzaAd.TabIndex = 0;
+            this.bVarostHozzaAd.Text = "Hozzá ad";
+            this.bVarostHozzaAd.UseVisualStyleBackColor = true;
+            this.bVarostHozzaAd.Click += new System.EventHandler(this.bVarostHozzaAd_Click);
+            // 
+            // tBVarosNev
+            // 
+            this.tBVarosNev.Location = new System.Drawing.Point(6, 19);
+            this.tBVarosNev.Name = "tBVarosNev";
+            this.tBVarosNev.Size = new System.Drawing.Size(100, 20);
+            this.tBVarosNev.TabIndex = 1;
+            // 
+            // nUADVarosLakosaga
+            // 
+            this.nUADVarosLakosaga.Location = new System.Drawing.Point(6, 45);
+            this.nUADVarosLakosaga.Name = "nUADVarosLakosaga";
+            this.nUADVarosLakosaga.Size = new System.Drawing.Size(120, 20);
+            this.nUADVarosLakosaga.TabIndex = 2;
+            // 
+            // gBVaros
+            // 
+            this.gBVaros.Controls.Add(this.tBVarosNev);
+            this.gBVaros.Controls.Add(this.nUADVarosLakosaga);
+            this.gBVaros.Controls.Add(this.bVarostHozzaAd);
+            this.gBVaros.Location = new System.Drawing.Point(490, 12);
+            this.gBVaros.Name = "gBVaros";
+            this.gBVaros.Size = new System.Drawing.Size(163, 111);
+            this.gBVaros.TabIndex = 3;
+            this.gBVaros.TabStop = false;
+            this.gBVaros.Text = "város felvétele";
+            // 
+            // gBLatvanyosag
+            // 
+            this.gBLatvanyosag.Controls.Add(this.tBLatvanyosagLeiras);
+            this.gBLatvanyosag.Controls.Add(this.nUDLatvanyosagAr);
+            this.gBLatvanyosag.Controls.Add(this.bLHozzaAd);
+            this.gBLatvanyosag.Controls.Add(this.cBVaroshozKotes);
+            this.gBLatvanyosag.Controls.Add(this.tBLatvanyosagNev);
+            this.gBLatvanyosag.Location = new System.Drawing.Point(135, 12);
+            this.gBLatvanyosag.Name = "gBLatvanyosag";
+            this.gBLatvanyosag.Size = new System.Drawing.Size(142, 173);
+            this.gBLatvanyosag.TabIndex = 4;
+            this.gBLatvanyosag.TabStop = false;
+            this.gBLatvanyosag.Text = "Látványoság felvétele";
+            // 
+            // nUDLatvanyosagAr
+            // 
+            this.nUDLatvanyosagAr.Location = new System.Drawing.Point(6, 71);
+            this.nUDLatvanyosagAr.Name = "nUDLatvanyosagAr";
+            this.nUDLatvanyosagAr.Size = new System.Drawing.Size(120, 20);
+            this.nUDLatvanyosagAr.TabIndex = 2;
+            // 
+            // bLHozzaAd
+            // 
+            this.bLHozzaAd.Location = new System.Drawing.Point(31, 134);
+            this.bLHozzaAd.Name = "bLHozzaAd";
+            this.bLHozzaAd.Size = new System.Drawing.Size(75, 23);
+            this.bLHozzaAd.TabIndex = 0;
+            this.bLHozzaAd.Text = "Hozzá ad";
+            this.bLHozzaAd.UseVisualStyleBackColor = true;
+            this.bLHozzaAd.Click += new System.EventHandler(this.bLHozzaAd_Click);
+            // 
+            // cBVaroshozKotes
+            // 
+            this.cBVaroshozKotes.FormattingEnabled = true;
+            this.cBVaroshozKotes.Location = new System.Drawing.Point(6, 97);
+            this.cBVaroshozKotes.Name = "cBVaroshozKotes";
+            this.cBVaroshozKotes.Size = new System.Drawing.Size(121, 21);
+            this.cBVaroshozKotes.TabIndex = 2;
+            // 
+            // tBLatvanyosagNev
+            // 
+            this.tBLatvanyosagNev.Location = new System.Drawing.Point(6, 19);
+            this.tBLatvanyosagNev.Name = "tBLatvanyosagNev";
+            this.tBLatvanyosagNev.Size = new System.Drawing.Size(100, 20);
+            this.tBLatvanyosagNev.TabIndex = 1;
+            // 
+            // tBLatvanyosagLeiras
+            // 
+            this.tBLatvanyosagLeiras.Location = new System.Drawing.Point(6, 45);
+            this.tBLatvanyosagLeiras.Name = "tBLatvanyosagLeiras";
+            this.tBLatvanyosagLeiras.Size = new System.Drawing.Size(100, 20);
+            this.tBLatvanyosagLeiras.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gBLatvanyosag);
+            this.Controls.Add(this.gBVaros);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nUADVarosLakosaga)).EndInit();
+            this.gBVaros.ResumeLayout(false);
+            this.gBVaros.PerformLayout();
+            this.gBLatvanyosag.ResumeLayout(false);
+            this.gBLatvanyosag.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nUDLatvanyosagAr)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bVarostHozzaAd;
+        private System.Windows.Forms.TextBox tBVarosNev;
+        private System.Windows.Forms.NumericUpDown nUADVarosLakosaga;
+        private System.Windows.Forms.GroupBox gBVaros;
+        private System.Windows.Forms.GroupBox gBLatvanyosag;
+        private System.Windows.Forms.TextBox tBLatvanyosagNev;
+        private System.Windows.Forms.NumericUpDown nUDLatvanyosagAr;
+        private System.Windows.Forms.Button bLHozzaAd;
+        private System.Windows.Forms.ComboBox cBVaroshozKotes;
+        private System.Windows.Forms.TextBox tBLatvanyosagLeiras;
+    }
+}
+
---
+++ LatvanyossagokApplication/Form1.cs
@@ -0,0 +1,211 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LatvanyossagokApplication
+{
+    public partial class Form1 : Form
+    {
+
+        MySqlConnection conn;
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new MySqlConnection("Server=localhost; Database=latvanyossagokdb; Uid=root; Pwd=;");
+            conn.Open();
+
+            this.FormClosed += (sender, args) =>
+            {
+                if (true)
+                {
+                    conn.Close();
+                }
+            };
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string sql = @"
+CREATE TABLE IF NOT EXISTS varosok(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nev VARCHAR(1000) UNIQUE NOT NULL,
+    lakossag INTEGER NOT NULL
+)
+";
+            var comm = this.conn.CreateCommand();
+            comm.CommandText = sql;
+            comm.ExecuteNonQuery();
+
+            string sql2 = @"
+CREATE TABLE IF NOT EXISTS latvanyossagok(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nev VARCHAR(1000) UNIQUE NOT NULL,
+    leiras VARCHAR(1000) NOT NULL,
+    ar INTEGER  DEFAULT 0  NOT NULL,
+    varos_id INTEGER,
+    FOREIGN KEY (varos_id)
+    REFERENCES varosok (id)
+)
+";
+            var comm2 = this.conn.CreateCommand();
+            comm2.CommandText = sql2;
+            comm2.ExecuteNonQuery();
+        }
+
+        private void bVarostHozzaAd_Click(object sender, EventArgs e)
+        {
+            if (!tBVarosNev.Text.Equals("") && !nUADVarosLakosaga.Value.Equals(0))
+            {
+                string sql = @"
+SELECT COUNT(nev) as varosKerese
+FROM varosok 
+WHERE nev LIKE @nev
+";
+                int nev;
+                var comm = this.conn.CreateCommand();
+                comm.CommandText = sql;
+                comm.Parameters.AddWithValue("@nev", tBVarosNev.Text);
+                using (var sherch = comm.ExecuteReader())
+                {
+                    sherch.Read();
+                    nev = sherch.GetInt32("varosKerese");
+                }
+                MessageBox.Show(nev + " ");
+
+                if (!(nev >= 1))
+                {
+                    string sql2 = @"
+INSERT INTO `varosok`(`nev`, `lakossag`)
+VALUES (@nev, @lakosag)
+";
+                    var comm2 = this.conn.CreateCommand();
+                    comm2.CommandText = sql2;
+                    comm2.Parameters.AddWithValue("@nev", tBVarosNev.Text);
+                    comm2.Parameters.AddWithValue("@lakosag", nUADVarosLakosaga.Value);
+                    comm2.ExecuteNonQuery();
+                }
+                else
+                {
+                    MessageBox.Show("ez a város már ben van az adattábázisabn.");
+                }
+            }
+            else if (tBVarosNev.Text.Equals("") && !nUADVarosLakosaga.Value.Equals(0))
+            {
+                MessageBox.Show("a város nevet ne adta meg");
+            }
+            else if (!tBVarosNev.Text.Equals("") && nUADVarosLakosaga.Value.Equals(0))
+            {
+                MessageBox.Show("a városnk biztos hogy nem 0 lakosa van");
+            }
+            else if (!tBVarosNev.Text.Equals("") && nUADVarosLakosaga.Value.Equals(0))
+            {
+                MessageBox.Show("a város nevet ne adta meg és biztos hogy nem 0 lakosa van");
+            }
+        }
+
+        private void bLHozzaAd_Click(object sender, EventArgs e)
+        {
+            cBVaroshozKotes.SelectedIndex = 0;
+            if (!tBLatvanyosagNev.Text.Equals("") && !tBLatvanyosagLeiras.Text.Equals("") && !nUDLatvanyosagAr.Value.Equals(0) &&  !(cBVaroshozKotes.SelectedIndex == 0))
+            {
+                string sql = @"
+SELECT COUNT(nev) as latvanyossagKerese
+FROM latvanyossagok 
+WHERE nev LIKE @nev
+";
+                int nev;
+                var comm = this.conn.CreateCommand();
+                comm.CommandText = sql;
+                comm.Parameters.AddWithValue("@nev", tBVarosNev.Text);
+                using (var sherch = comm.ExecuteReader())
+                {
+                    sherch.Read();
+                    nev = sherch.GetInt32("latvanyossagKerese");
+                }
+                MessageBox.Show(nev + " ");
+
+                if (!(nev >= 1))
+                {
+                    string sql2 = @"
+INSERT INTO `varosok`(`nev`, `lakossag`)
+VALUES (@nev, @lakosag)
+";
+                    var comm2 = this.conn.CreateCommand();
+                    comm2.CommandText = sql2;
+                    comm2.Parameters.AddWithValue("@nev", tBVarosNev.Text);
+                    comm2.Parameters.AddWithValue("@lakosag", nUADVarosLakosaga.Value);
+                    comm2.ExecuteNonQuery();
+                }
+                else
+                {
+                    MessageBox.Show("ez a város már ben van az adattábázisabn.");
+                }
+            }
+            else if (tBLatvanyosagNev.Text.Equals("") && tBLatvanyosagLeiras.Text.Equals("") && nUDLatvanyosagAr.Value.Equals(0) && (cBVaroshozKotes.SelectedIndex == 0))
+            {
+                MessageBox.Show("a látványoság nevet, leirását, árát ne adta meg és nem köttöte városhoz.");
+            }
+            else if (!tBLatvanyosagNev.Text.Equals("") && tBLatvanyosagLeiras.Text.Equals("") && nUDLatvanyosagAr.Value.Equals(0) && (cBVaroshozKotes.SelectedIndex == 0))
+            {
+                MessageBox.Show("a látványoság leirását, árát ne adta meg és nem köttöte városhoz.");
+            }
+            else if (tBLatvanyosagNev.Text.Equals("") && tBLatvanyosagLeiras.Text.Equals("") && nUDLatvanyosagAr.Value.Equals(0) && !(cBVaroshozKotes.SelectedIndex == 0))
+            {
+                MessageBox.Show("a látványoság nevet, leirását és árát ne adta meg és nem köttöte városhoz.");
+            }
+            else if (tBLatvanyosagNev.Text.Equals("") && !tBLatvanyosagLeiras.Text.Equals("") && nUDLatvanyosagAr.Value.Equals(0) && (cBVaroshozKotes.SelectedIndex == 0))
+            {
+                MessageBox.Show("a látványoság nevet, árát ne adta meg és nem köttöte városhoz.");
+            }
+            else if (!tBLatvanyosagNev.Text.Equals("") && !tBLatvanyosagLeiras.Text.Equals("") && nUDLatvanyosagAr.Value.Equals(0) && (cBVaroshozKotes.SelectedIndex == 0))
+            {
+                MessageBox.Show("a látványoság árát ne adta meg és nem köttöte városhoz.");
+            }
+            else if (!tBLatvanyosagNev.Text.Equals("") && tBLatvanyosagLeiras.Text.Equals("") && nUDLatvanyosagAr.Value.Equals(0) && !(cBVaroshozKotes.SelectedIndex == 0))
+            {
+                MessageBox.Show("a látványoság leirását és árát ne adta meg");
+            }
+            else if (!tBLatvanyosagNev.Text.Equals("") && tBLatvanyosagLeiras.Text.Equals("") && !nUDLatvanyosagAr.Value.Equals(0) && (cBVaroshozKotes.SelectedIndex == 0))
+            {
+                MessageBox.Show("a látványoság leirását ne adta meg és nem köttöte városhoz.");
+            }
+            else if (tBLatvanyosagNev.Text.Equals("") && tBLatvanyosagLeiras.Text.Equals("") && !nUDLatvanyosagAr.Value.Equals(0) && !(cBVaroshozKotes.SelectedIndex == 0))
+            {
+                MessageBox.Show("a látványoság nevet és leirását ne adta meg");
+            }
+            else if (tBLatvanyosagNev.Text.Equals("") && !tBLatvanyosagLeiras.Text.Equals("") && nUDLatvanyosagAr.Value.Equals(0) && !(cBVaroshozKotes.SelectedIndex == 0))
+            {
+                MessageBox.Show("a látványoság nevet és árát ne adta meg");
+            }
+            else if (tBLatvanyosagNev.Text.Equals("") && !tBLatvanyosagLeiras.Text.Equals("") && !nUDLatvanyosagAr.Value.Equals(0) && (cBVaroshozKotes.SelectedIndex == 0))
+            {
+                MessageBox.Show("a látványoság nevet ne adta meg és nem köttöte városhoz.");
+            }
+            else if (tBLatvanyosagNev.Text.Equals("") && !tBLatvanyosagLeiras.Text.Equals("") && !nUDLatvanyosagAr.Value.Equals(0) && !(cBVaroshozKotes.SelectedIndex == 0))
+            {
+                MessageBox.Show("a látványoság nevet ne adta meg");
+            }
+            else if (!tBLatvanyosagNev.Text.Equals("") && tBLatvanyosagLeiras.Text.Equals("") && !nUDLatvanyosagAr.Value.Equals(0) && !(cBVaroshozKotes.SelectedIndex == 0))
+            {
+                MessageBox.Show("a látványoság leirását ne adta meg");
+            }
+            else if (!tBLatvanyosagNev.Text.Equals("") && !tBLatvanyosagLeiras.Text.Equals("") && nUDLatvanyosagAr.Value.Equals(0) && !(cBVaroshozKotes.SelectedIndex == 0))
+            {
+                MessageBox.Show("a látványoság biztos hogy 0 ft-be kerül");
+            }
+            else if (!tBLatvanyosagNev.Text.Equals("") && !tBLatvanyosagLeiras.Text.Equals("") && !nUDLatvanyosagAr.Value.Equals(0) && (cBVaroshozKotes.SelectedIndex == 0))
+            {
+                MessageBox.Show("a látványoságot nem köttöte városhoz.");
+            }
+        }
+    }
+}
